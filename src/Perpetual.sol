63d782468da34fc473dbea502ec986d40242b7a8
palmcivet7 <palmcivet@proton.me>
2023-12-15 00:04:15 +0000

    import IERC20 and start user functions

--- src/Perpetual.sol
+++ src/Perpetual.sol
@@ -5,19 +5,37 @@ pragma solidity 0.8.19;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {IVault} from "./interfaces/IVault.sol";
 import {Vault} from "./Vault.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Perpetual {
     error Perpetual__InvalidAddress();
 
+    using SafeERC20 for IERC20;
+
     AggregatorV3Interface public immutable i_priceFeed;
     IVault public immutable i_vault;
+    IERC20 public immutable i_collateralToken;
 
-    constructor(address _priceFeed, address _vault) {
+    constructor(address _priceFeed, address _vault, address _collateralToken) {
         if (_priceFeed == address(0)) revert Perpetual__InvalidAddress();
         if (_vault == address(0)) revert Perpetual__InvalidAddress();
+        if (_collateralToken == address(0)) revert Perpetual__InvalidAddress();
         i_priceFeed = AggregatorV3Interface(_priceFeed);
         i_vault = IVault(_vault);
+        i_collateralToken = IERC20(_collateralToken);
     }
+
+    /////////////////////
+    ////// User ////////
+    ///////////////////
+
+    function openPosition(uint256 _size, uint256 _collateralAmount) external {
+        uint256 leverage = _size / _collateralAmount;
+    }
+
+    function increaseSize() external {}
+
+    function increaseCollateral() external {}
 
     /////////////////////
     ////// Getter //////
