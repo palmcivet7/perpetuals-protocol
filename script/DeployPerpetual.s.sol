02fae6ac146405dfbc682a9db3612f6238ede7aa
palmcivet7 <palmcivet@proton.me>
2023-12-25 04:12:33 +0000

    format deploy script

--- script/DeployPerpetual.s.sol
+++ script/DeployPerpetual.s.sol
@@ -9,12 +9,10 @@ import {HelperConfig} from "../script/HelperConfig.s.sol";
 contract DeployPerpetual is Script {
     function run() external returns (Perpetual, HelperConfig) {
         HelperConfig config = new HelperConfig();
-
         (address priceFeed, address vault, address asset) = config.activeNetworkConfig();
 
         vm.startBroadcast();
         Perpetual perpetual = new Perpetual(priceFeed, vault, asset);
-
         vm.stopBroadcast();
         return (perpetual, config);
     }
