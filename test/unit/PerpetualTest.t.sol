b76e44ea476349ae8142a4cd5db47e217406e0ea
palmcivet7 <palmcivet@proton.me>
2023-12-29 00:42:39 +0000

    begin unit tests

---
+++ test/unit/PerpetualTest.t.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployPerpetual} from "../../script/DeployPerpetual.s.sol";
+import {Perpetual} from "../../src/Perpetual.sol";
+import {Vault} from "../../src/Vault.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {MockUSDC} from "../../src/mocks/MockUSDC.sol";
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+
+contract PerpetualTest is Test {
+    Perpetual perpetual;
+    Vault vault;
+    HelperConfig helperConfig;
+    MockUSDC mockUsdc;
+    MockV3Aggregator priceFeed;
+
+    address public LIQUIDITY_PROVIDER = makeAddr("LIQUIDITY_PROVIDER");
+    address public TRADER = makeAddr("TRADER");
+    uint256 public constant STARTING_USER_BALANCE = 1000 ether;
+    uint256 public constant ONE_THOUSAND_USDC = 1000_000000;
+
+    function setUp() external {
+        DeployPerpetual deployer = new DeployPerpetual();
+        (perpetual, helperConfig) = deployer.run();
+        (address priceFeedAddress, address vaultAddress, address assetAddress) = helperConfig.activeNetworkConfig();
+        priceFeed = MockV3Aggregator(priceFeedAddress);
+        vault = Vault(vaultAddress);
+        mockUsdc = MockUSDC(assetAddress);
+
+        vm.prank(address(helperConfig));
+        vault.setPerpetual(address(perpetual));
+
+        vm.deal(LIQUIDITY_PROVIDER, STARTING_USER_BALANCE);
+        vm.deal(TRADER, STARTING_USER_BALANCE);
+        vm.prank(LIQUIDITY_PROVIDER);
+        mockUsdc.mintTokens(ONE_THOUSAND_USDC);
+        vm.prank(TRADER);
+        mockUsdc.mintTokens(ONE_THOUSAND_USDC);
+    }
+
+    function test_constructor_sets_values() public {
+        assertEq(address(priceFeed), address(perpetual.i_priceFeed()));
+        assertEq(address(vault), address(perpetual.i_vault()));
+        assertEq(address(mockUsdc), address(perpetual.i_collateralToken()));
+    }
+
+    function test_openPosition_reverts_if_size_is_zero() public {
+        vm.startPrank(TRADER);
+        vm.expectRevert(Perpetual.Perpetual__InvalidValue.selector);
+        perpetual.openPosition(0, 1, true);
+        vm.stopPrank();
+    }
+
+    function test_openPosition_reverts_if_collateralAmount_is_zero() public {
+        vm.startPrank(TRADER);
+        vm.expectRevert(Perpetual.Perpetual__InvalidValue.selector);
+        perpetual.openPosition(1, 0, true);
+        vm.stopPrank();
+    }
+
+    function test_openPosition_reverts_if_liquidity_is_insufficient() public {
+        vm.startPrank(TRADER);
+        vm.expectRevert(Perpetual.Perpetual__InsufficientLiquidity.selector);
+        perpetual.openPosition(1, 1, true);
+        vm.stopPrank();
+    }
+
+    modifier liquidityDeposited() {
+        vm.startPrank(LIQUIDITY_PROVIDER);
+        mockUsdc.approve(address(vault), ONE_THOUSAND_USDC);
+        vault.deposit(ONE_THOUSAND_USDC, LIQUIDITY_PROVIDER);
+        vm.stopPrank();
+        _;
+    }
+
+    function test_openPosition_works() public liquidityDeposited {
+        vm.startPrank(msg.sender);
+        uint256 vaultBalanceOf = vault.balanceOf(LIQUIDITY_PROVIDER);
+        console.log("vaultBalanceOf:", vaultBalanceOf);
+        vm.stopPrank();
+    }
+}
