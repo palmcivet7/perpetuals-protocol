42644243f528e5f6958b477633ff1a01ba74aef8
palmcivet7 <palmcivet@proton.me>
2024-07-12 21:01:25 +0100

    layout initial external functions

--- src/Positions.sol
+++ src/Positions.sol
@@ -4,4 +4,14 @@ pragma solidity 0.8.24;
 
 import {IPositions} from "./interfaces/IPositions.sol";
 
-contract Positions is IPositions {}
+contract Positions is IPositions {
+    function openPosition() external {}
+
+    function increaseSize() external {}
+
+    function increaseCollateral() external {}
+
+    function decreaseSize() external {}
+
+    function decreaseCollateral() external {}
+}
