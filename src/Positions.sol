46091e708646574910f678113895d62dbc17b881
palmcivet7 <palmcivet@proton.me>
2024-07-12 21:04:26 +0100

    import ERC20 contracts and link pricefeed aggregator

--- src/Positions.sol
+++ src/Positions.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.24;
 
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {IPositions} from "./interfaces/IPositions.sol";
 
 contract Positions is IPositions {
