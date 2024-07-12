40ca096e546f2e72c9be21cf61dbe6b193f57ce0
palmcivet7 <palmcivet@proton.me>
2024-07-12 23:54:15 +0100

    test vault withdraw

--- test/Positions.t.sol
+++ test/Positions.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {Positions, IPositions} from "../src/Positions.sol";
 import {Vault, IVault} from "../src/Vault.sol";
 import {MockV3Aggregator} from "./mocks/MockV3Aggregator.sol";
@@ -21,6 +21,9 @@ contract PositionsTest is Test {
     uint256 constant ONE_USDC = 1_000_000;
     uint256 constant WAD = 1e18;
 
+    /*//////////////////////////////////////////////////////////////
+                                 SETUP
+    //////////////////////////////////////////////////////////////*/
     function setUp() public {
         usdc = new MockUsdc();
         priceFeed = new MockV3Aggregator(8, 2000_00000000); // index tokens are initially worth $2k
@@ -40,6 +43,9 @@ contract PositionsTest is Test {
         _;
     }
 
+    /*//////////////////////////////////////////////////////////////
+                             OPEN POSITION
+    //////////////////////////////////////////////////////////////*/
     function test_openPosition() public liquidityDeposited {
         uint256 sizeInTokenAmount = 0.5 ether; // half an index token
 
@@ -47,5 +53,54 @@ contract PositionsTest is Test {
         usdc.approve(address(positions), FIFTY_USDC);
         positions.openPosition(sizeInTokenAmount, FIFTY_USDC, true);
         vm.stopPrank();
+    }
+
+    function test_openPosition_reverts_if_max_leverage_exceeded() public liquidityDeposited {
+        uint256 sizeInTokenAmount = 2 ether;
+
+        vm.startPrank(trader);
+        usdc.approve(address(positions), FIFTY_USDC);
+        vm.expectRevert(Positions.Positions__MaxLeverageExceeded.selector);
+        positions.openPosition(sizeInTokenAmount, FIFTY_USDC, true);
+        vm.stopPrank();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             VAULT WITHDRAW
+    //////////////////////////////////////////////////////////////*/
+    function test_lp_can_withdraw() public liquidityDeposited {
+        uint256 maxWithdraw = vault.maxWithdraw(liquidityProvider);
+        vm.prank(liquidityProvider);
+        vault.withdraw(maxWithdraw, liquidityProvider, liquidityProvider);
+    }
+
+    function test_lp_cant_withdraw_reserved_liquidity() public liquidityDeposited {
+        uint256 sizeInTokenAmount = 0.01 ether;
+        vm.startPrank(trader);
+        usdc.approve(address(positions), FIFTY_USDC);
+        positions.openPosition(sizeInTokenAmount, FIFTY_USDC, true);
+        vm.stopPrank();
+
+        uint256 availableLiquidityBeforePriceChange = positions.getAvailableLiquidity();
+        console.log("availableLiquidityBeforePriceChange:", availableLiquidityBeforePriceChange);
+
+        priceFeed.updateAnswer(5000_00000000);
+
+        uint256 expectedWithdrawnAmount = positions.getAvailableLiquidity();
+        console.log("expectedWithdrawnAmount:", expectedWithdrawnAmount);
+
+        assert(availableLiquidityBeforePriceChange != expectedWithdrawnAmount);
+        assertGt(availableLiquidityBeforePriceChange, expectedWithdrawnAmount);
+        assertGt(expectedWithdrawnAmount, 0);
+
+        uint256 lpBalanceStart = usdc.balanceOf(liquidityProvider);
+        uint256 maxWithdraw = vault.maxWithdraw(liquidityProvider);
+
+        vm.prank(liquidityProvider);
+        vault.withdraw(maxWithdraw, liquidityProvider, liquidityProvider);
+
+        uint256 lpBalanceEnd = usdc.balanceOf(liquidityProvider);
+        uint256 actualWithdrawnAmount = lpBalanceEnd - lpBalanceStart;
+        assertEq(expectedWithdrawnAmount, actualWithdrawnAmount);
     }
 }
