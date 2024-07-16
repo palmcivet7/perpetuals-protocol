986a339c370c0b2df52b86187d3249ca0a4c6879
palmcivet7 <palmcivet@proton.me>
2024-07-16 10:19:06 +0100

    start liquidator deploy script

---
+++ script/DeployAutomatedLiquidator.s.sol
@@ -0,0 +1,8 @@
+// // SPDX-License-Identifier: MIT
+
+// pragma solidity 0.8.24;
+
+// import {Script} from "forge-std/Script";
+// import {AutomatedLiquidator} from "../src/AutomatedLiquidator.sol";
+
+// contract DeployAutomatedLiquidator is Script {}
